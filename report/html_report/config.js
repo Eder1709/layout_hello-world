01f50b01627b7f43105562031ceb78401b15db66
Seu Nome <seu-email@example.com>
2025-07-21 15:43:57 -0300

    make build

--- report/html_report/config.js
+++ report/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Entire_document.png",
-        "test": "..\\bitmaps_test\\20250721-151406\\Entire_document.png",
+        "test": "..\\bitmaps_test\\20250721-152122\\Entire_document.png",
         "selector": "document",
         "fileName": "Entire_document.png",
         "label": "Entire document",
